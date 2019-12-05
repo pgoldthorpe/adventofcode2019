3ea5f82ea42471cf4c2322541e6f9b515813bed1
pgoldthorpe <43900257+pgoldthorpe@users.noreply.github.com>
2019-12-06 01:44:42 +1100

    Changed from list to dict for 4x speed improvement

--- solution/solution03.py
+++ solution/solution03.py
@@ -1,41 +1,44 @@
 def main():
   with open('../input/input03.txt','r') as f:
-    wire1, wire2, _ = [i for i in f.read().split('\n')]
-    wire1 = wire1.split(',')
-    wire2 = wire2.split(',')
+    wire1_instructions, wire2_instructions, _ = [i for i in f.read().split('\n')]
+    wire1_instructions = wire1_instructions.split(',')
+    wire2_instructions = wire2_instructions.split(',')
 
   def points(wire):
     x,y = 0,0
-    series = [(0,0)]
+    series = {(0,0): 0}
+    length = 0
     
     for item in wire:
       direction, steps = item[0], int(item[1:])
       if direction == 'R':
         for i in range(steps):
+          length+=1
           x+=1
-          series.append((x,y))
+          series.setdefault((x,y), length)
       if direction == 'U':
         for i in range(steps):
+          length+=1
           y+=1
-          series.append((x,y))
+          series.setdefault((x,y), length)
       if direction == 'L':
         for i in range(steps):
+          length+=1
           x-=1
-          series.append((x,y))
+          series.setdefault((x,y), length)
       if direction == 'D':
         for i in range(steps):
+          length+=1
           y-=1
-          series.append((x,y))
+          series.setdefault((x,y), length)
     return series
 
-  wire1points = points(wire1)
-  wire2points = points(wire2)
+  wire1_points = points(wire1_instructions)
+  wire2_points = points(wire2_instructions)
 
-  collision_points = set(wire1points).intersection(set(wire2points)) - {(0,0)}
-  solution1 = min([abs(i[0]) + abs(i[1]) for i in collision_points])
-
-  steps = {wire1points.index(point) + wire2points.index(point) for point in collision_points}
-  solution2 = min(steps)
+  collision_points = set(wire1_points).intersection(set(wire2_points)) - {(0,0)}
+  solution1 = min({abs(i[0]) + abs(i[1]) for i in collision_points})
+  solution2 = min({wire1_points[i] + wire2_points[i] for i in collision_points})
 
   if __name__ == "__main__":
     print("Solution to problem 1 is {}".format(solution1))
